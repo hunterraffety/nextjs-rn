502993ad60b8bec4084c7509c893fdb695da09a8
Hunter Raffety <hsraffety@gmail.com>
2020-07-04 07:42:38 -0700

    adds tailwind, configures build to use.

--- components/sidemenu.js
+++ components/sidemenu.js
@@ -1,6 +1,7 @@
 const SideMenu = ({ count, clickHandler, appName }) => {
   return (
     <div className="sideMenuContainer">
+      <button onClick={clickHandler}>click</button>
       <h1 className="my-4">{appName}</h1>
       <div className="list-group">
         <a href="#" className="list-group-item">
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/sass/styles.scss'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
